e681cd89ea7fd3279dc392c49a39e2e5c400dab7
Jupiter <kjimin2123@gmail.com>
2024-12-30 18:40:47 +0900

    feat(entity): Add gender enum and enhance user and address entities
    - 성별을 나타내는 Gender 열거형을 추가했습니다.
    - 사용자 엔티티에서 성별 필드를 boolean에서 Gender 열거형으로 변경했습니다.
    - 주소 엔티티에 기본 주소 설정 및 해제 메서드를 추가했습니다.
    - 사용자 생성 및 업데이트 DTO에 성별 필드를 추가하고 유효성 검사를 적용했습니다.
    - 로컬 계정 엔티티에 계정 잠금 기능과 관련된 메서드를 추가했습니다.
    - 로컬 계정 로그인 기능 부분에 계정 자금 기능과 관련된 기능을 추가했습니다.
    
    Linked to #32

--- src/entites/local-account.entity.ts
+++ src/entites/local-account.entity.ts
@@ -1,5 +1,6 @@
-import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { BeforeInsert, BeforeUpdate, Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { User } from './user.entity';
+import { hashPassword } from 'src/common/utils/hash.util';
 
 @Entity()
 export class LocalAccount {
@@ -18,7 +19,63 @@ export class LocalAccount {
   @Column({ default: 0 })
   loginAttempt: number; // 로그인 시도 횟수
 
+  @Column({ default: false })
+  isLocked: boolean; // 계정 잠금 상태
+
+  @Column({ nullable: true })
+  lockedAt: Date; // 계정 잠금 시간
+
   @OneToOne(() => User, (user) => user.localAccount, { onDelete: 'CASCADE' })
   @JoinColumn()
   user: User;
+
+  // 비밀번호 암호화를 위한 메서드
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword(): Promise<void> {
+    if (this.password && !this.password.startsWith('$2b$')) {
+      this.password = await hashPassword(this.password);
+    }
+  }
+
+  /**
+   * 계정 잠금 여부 확인 메서드
+   * @returns {boolean} 계정 잠금 여부
+   */
+  isAccountLocked(): boolean {
+    if (this.isLocked && this.lockedAt) {
+      const lockDurations = [5, 15, 30]; // 단위: 분, 잠금 지속 시간 (예: 5분, 15분, 30분)
+      const duration = lockDurations[Math.min(this.loginAttempt, lockDurations.length) - 1] || 30;
+      const lockExpiry = new Date(this.lockedAt.getTime() + duration * 60 * 1000);
+      if (new Date() > lockExpiry) {
+        this.isLocked = false;
+        this.lockedAt = null;
+        this.loginAttempt = 0;
+      }
+    }
+    return this.isLocked;
+  }
+
+  /**
+   * 로그인 실패 처리 메서드
+   * @param {number} maxAttempts 최대 시도 횟수
+   * @returns {void}
+   */
+  handleFailedLogin(maxAttempts: number): void {
+    this.loginAttempt += 1;
+    if (this.loginAttempt >= maxAttempts) {
+      this.isLocked = true;
+      this.lockedAt = new Date();
+    }
+  }
+
+  /**
+   * 로그인 성공 처리 메서드
+   * @returns {void}
+   */
+  resetLoginAttempts(): void {
+    this.loginAttempt = 0;
+    this.isLocked = false;
+    this.lockedAt = null;
+  }
 }
